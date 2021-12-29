aacda35b689a978b65fa22967b8a0efd7826de51
shaidsifat <shaidsifat55@gmail.com>
2021-12-29 08:33:17 +0600

    added mycode

--- mobile/urls.py
+++ mobile/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from mobile.views import About
+
 
 urlpatterns = [
 
     path('product/', views.home_view, name='mobile'),
+    path('all/', About.as_view(), name="allselect"),
 ]
--- mobile/views.py
+++ mobile/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render
 from .forms import Form
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
 
 def home_view(request):
+    form = Form()
+    if request.method == "POST":
+        form = Form(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/')
+            except:
+                pass
+        else:
+            form = Form()
+            return render(request, 'index.html', {'form': form})
 
+    return render(request, 'index.html', {'form': form})
+
+
+class About(TemplateView):
+    template_name = 'home.html'
+
+
+'''
+    if request.method == "POST":
+        form = Form(request.POST)
+        if form.is_valid():
+            try:
+                return redirect('/')
+            except:
+                pass
+        else:
+            form = Form()
+            return render(request, 'index.html', {'form': form})
     stu = Form()
-    return render(request, "index.html", {'form': stu})
+    return render(request, "index.html", {'form': form})
+'''
